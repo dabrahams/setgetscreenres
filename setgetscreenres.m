1c9f0444f66dc6960e5461d7a6bd9069ac75bbf4
Juan FC <juanfc@unknown.net>
2012-03-08 10:21:00 -0600

    Change screen resolution in Terminal
    http://hints.macworld.com/article.php?story=20090413120929454
    
    Terminal is very useful in managing of rooms of Macs with Apple Remote Desktop. One thing I do often is change the screen resolution. To do that, I use a custom Terminal program that's based largely on Jeffrey Osterman's code posted in this Mac OS X Hints Forums thread.
    
    It is easy to compile (you'll need Xcode to do so, however), and it supersedes the old and missing cscreen. First, here's the code:
    
    Save that as a pure text file named setgetscreenres.m, then compile it in Terminal using the command shown in the code (assuming you're in the directory where the file is saved):
    c++ setgetscreenres.m -framework ApplicationServices -o setgetscreenres
    Move this file somewhere on your user's $PATH, and then use it by typing setgetscreenres hor_res vert_res, where hor_res and vert_res are the desire horizontal and vertical resolution.

--- setgetscreenres.m
+++ setgetscreenres.m
@@ -1,62 +1,62 @@
 /*
- * main.c
- * newscreen
- *
- * Created by Jeffrey Osterman on 10/30/07.
- * Copyright 2007 Jeffrey Osterman. All rights reserved. 
- * PROVIDED AS IS AND WITH NO WARRANTIES WHATSOEVER
- */
+* setgetscreenres.m
+* 
+* juanfc 2009-04-13
+* Based on newscreen
+*    Created by Jeffrey Osterman on 10/30/07.
+*    Copyright 2007 Jeffrey Osterman. All rights reserved. 
+*    PROVIDED AS IS AND WITH NO WARRANTIES WHATSOEVER
+*    http://forums.macosxhints.com/showthread.php?t=59575
+*
+* COMPILE:
+*    c++ setgetscreenres.m -framework ApplicationServices -o setgetscreenres
+* USE:
+*    setgetscreenres 1440 900
+*/
 
 #include <ApplicationServices/ApplicationServices.h>
 
-void MyDisplaySwitchToMode (CGDirectDisplayID display, CFDictionaryRef mode);
+bool MyDisplaySwitchToMode (CGDirectDisplayID display, CFDictionaryRef mode);
 
-int main (int argc, const char * argv[]) {
+int main (int argc, const char * argv[])
+{
+    int h;                          // horizontal resolution
+    int v;                          // vertical resolution
+    CFDictionaryRef switchMode;     // mode to switch to
+    CGDirectDisplayID mainDisplay;  // ID of main display
 
-    int	h; //horizontal resolution
-    int v; //vertical resolution
-    CFDictionaryRef switchMode; //mode to swich to
-    CGDirectDisplayID mainDisplay; //ID of main display
+    CFDictionaryRef CGDisplayCurrentMode(CGDirectDisplayID display);
 
-
-    if (argc != 5)
-    {
-        printf("Expected 4 inputs, but only received %i\n", argc-1);
+    if (argc == 1) {
+        CGRect screenFrame = CGDisplayBounds(kCGDirectMainDisplay);
+        CGSize screenSize  = screenFrame.size;
+        printf("%d %d\n", screenSize.width, screenSize.height);
         return 0;
     }
-
-    if (!strcmp(argv[1],"-h"))
-        h=strtol(argv[2], NULL, 0);
-    else if (!strcmp(argv[1],"-v"))
-        v=strtol(argv[2], NULL, 0);
-    else {
-        printf("Error! Expected first argument to be -h or -v\n");
-        return 0;
-    }
-
-
-    if (!strcmp(argv[3],"-h"))
-        h=strtol(argv[4], NULL, 0);
-    else if (!strcmp(argv[3],"-v"))
-        v=strtol(argv[4], NULL, 0);
-    else {
-        printf("Error! Expected third argument to be -h or -v");
-        return 0;
+    if (argc != 3 || !(h = atoi(argv[1])) || !(v = atoi(argv[2])) ) {
+        fprintf(stderr, "ERROR: Use %s horres vertres\n", argv[0]);
+        return -1;
     }
 
     mainDisplay = CGMainDisplayID();
+
     switchMode = CGDisplayBestModeForParameters(mainDisplay, 32, h, v, NULL);
-    MyDisplaySwitchToMode (mainDisplay, switchMode);
+
+    if (! MyDisplaySwitchToMode(mainDisplay, switchMode)) {
+        fprintf(stderr, "Error changing resolution to %d %d\n", h, v);
+        return 1;
+    }
+
     return 0;
 }
 
-
-
-void MyDisplaySwitchToMode (CGDirectDisplayID display, CFDictionaryRef mode)
+bool MyDisplaySwitchToMode (CGDirectDisplayID display, CFDictionaryRef mode)
 {
-    CGDisplayConfigRef config; // 1
-    CGBeginDisplayConfiguration (&config); // 2
-    CGConfigureDisplayMode (config, display, mode); // 3
-
-    CGCompleteDisplayConfiguration (config, kCGConfigureForSession ); // 5
+    CGDisplayConfigRef config;
+    if (CGBeginDisplayConfiguration(&config) == kCGErrorSuccess) {
+        CGConfigureDisplayMode(config, display, mode);
+        CGCompleteDisplayConfiguration(config, kCGConfigureForSession );
+        return true;
+    }
+    return false;
 }
